046e2842a9cf9930f4a426c1cb7e6ef31319b7dd
niklas <niklashainz@gmail.com>
2026-06-09 20:52:11 +0200

    final push

--- godot/scripts/game_state.gd
+++ godot/scripts/game_state.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 func game_start():
 	var player = get_parent().get_node("Node2D").get_node("Player")
-	player.set_health(500)
+	player.set_health(5)
 	
 
 func game_end():
